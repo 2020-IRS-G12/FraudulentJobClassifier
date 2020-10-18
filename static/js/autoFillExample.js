9ad90229d9f4b2fcb7593f4e5db5d712914908ea
Your Name <you@example.com>
2020-10-19 01:16:56 +0800

    feat: add showcase buttons to autofill form for demo

---
+++ static/js/autoFillExample.js
@@ -0,0 +1,70 @@
+function fillExample1(){
+  $("#title").val("Manager of Support Services");
+  $("#department").val("Cloud Services");
+  $("#employmentType").val("Full-time");
+  $("#requiredExp").val("Mid-Senior level");
+  $("#requiredEdu").val("Bachelor's Degree");
+  $("#function").val("Information Technology");
+  $("#industry").val("Internet");
+  $("#companyProfile").val("GetCloudServices is a privately held technology services firm founded in 2003. We are a leader in the professional cloud services market with an unwavering dedication to providing the best hosting services available, regardless of platform. We integrate best-in-class connectivity and technology into a fully-automated platform which empowers enterprises with complete access, control, security and scalability. Our mission is to deliver superior technology to businesses that could not acquire it on their own. We are known for exceptional customer service that excels at simplifying complex technologies and has established a brand people trust. GetCloudServices operates primarily from a world-class data center in Jacksonville, Florida and helps businesses around the globe with advanced online needs. We provide Cloud Desktop, VDI, managed Cloud Services, and Cloud Server Hosting in a carrier-neutral facility. We are a Drug-Free Workplace and an EEO Employer.");
+  $("#desc").val("GetCloudServices is a privately held technology services firm founded in 2003. We are a leader in the professional cloud services market with an unwavering dedication to providing the best hosting services available, regardless of platform. We integrate best-in-class connectivity and technology into a fully-automated platform which empowers enterprises with complete access, control, security and scalability. Our mission is to deliver superior technology to businesses that could not acquire it on their own. We are known for exceptional customer service that excels at simplifying complex technologies and has established a brand people trust. GetCloudServices operates primarily from a world-class data center in Jacksonville, Florida and helps businesses around the globe with advanced online needs. We provide Cloud Desktop, VDI, managed Cloud Services, and Cloud Server Hosting in a carrier-neutral facility. We are a Drug-Free Workplace and an EEO Employer.PositionPlease note this is a telecommuting position. You must have a high speed Internet connection and quiet work environment.We are looking for a Manager of Support Services to help lead team of talented Technical Support Specialists. You would be responsible for the supervision, measuring, monitoring, enforcement, and compliance, while raising the standards and improving the performance of this critical team. With your leadership, your team will be responsible for anticipating, identifying and satisfying the technical support and services needs of our customers. Working collaboratively with peer teams in technical support and engineering, as well as with other stakeholders, you will ensure that our clients are achieving maximum value from our solutions. The Manager of Support Services will apply their knowledge of customer support best practices, industry standards, and the skills and experiences of attaining highest customer satisfaction to elevate our customer service culture and performance.ResponsibilitiesOversee End User Services to ensure that end user problems are resolved in a timely and effective manner, enabling users to access needed information and utilize technology resources effectively.Manage, train and develop a strong support staff to ensure a high level of customer satisfaction.Direct the team workflow and customer interactions.Manage the work schedule for the team to ensure appropriate resource allocationAct as a point of escalation to resolve complex issues and/or reassign cases to ensure rapid resolution to customer issues.Coordinate with and maintain relationships with cross-functional teams to ensure efficient operations.Communicate effectively with customers and stakeholders to assess support needs, assist in the identification of technology needs, and respond to customer service concerns.Responsible for process documentation and process development.Establish and monitor service level targets/benchmarks and measure performance against those benchmarks.Measure and report on unit performance via metrics and indicators of service level activity and customer satisfaction. Provide regular helpdesk performance and utilization reports to leadership.Analyze performance statistics on a weekly basis and provide recommendations for process improvement in support of team metrics.Track and analyze end user issues, response and resolution rates to identify areas of need and create strategies to enhance end-user capacity and end-user reliance on support personnel.Develop and maintain comprehensive documentation.Responsible for organizing and conducting user level training and overview for GetCloudServices computer systems and applications.RequirementsMust be a resident of Florida or willing to relocate to Florida.Bachelor`s degree in a Technical Discipline preferred.Previous experience managing and building technical support teams of 5 or more.Ability to excite and inspire your team.Proven track record of managing customer expectations and ensuring the highest level of customer satisfaction.Experience driving change in a fast paced technical support environment.Excellent verbal and written communication skills. Must be able to handle a large volume of client interactions through written and verbal methods.Qualifications:Desired Qualifications (not required but desired)Industry certifications, preferably MCSA, MSCE, MCTS, MCITP, VCP, VCAData center experience, including cabling, racking, etc.Experience with Windows Active Directory Experience with virtualization technologies, specifically VMware ESXI Experience with hardware and software firewalls Experience using or managing a hosted environment Experience with network security concepts");
+  $("#benefits").val("");
+  $("#requirements").val("");
+
+  $("#hasQuestion").find("input").removeAttr("checked");
+  $("#hasLogo").find("input").removeAttr("checked");
+  $("#telecommuting").find("input").removeAttr("checked");
+
+  $("#hasQuestion").find("input[value=1]").prop("checked", true);
+  $("#hasLogo").find("input[value=1]").prop("checked", true);
+  $("#telecommuting").find("input[value=1]").prop("checked", true);
+
+}
+
+function fillExample2(){
+  $("#title").val("Product Support Specialist");
+  $("#department").val("");
+  $("#employmentType").val("Full-time");
+  $("#requiredExp").val("Entry level");
+  $("#requiredEdu").val("Unspecified");
+  $("#function").val("Customer Service");
+  $("#industry").val("Information Technology and Services");
+  $("#companyProfile").val("EROAD was established to modernise New Zealand paper-based RUC regime and in 2009 launched the world first GPS/cellular-based road charging system.Our solution can be readily and rapidly scaled and deployed across entire jurisdictions because it requires no roadside architecture, and uses a SaaS-based web service.We now a world leader in our field and expanding rapidly as we enter new markets.Joining EROAD is a great career move. We look at potential when we hiring, and your ability to grow with the role. We employ only the best, and ensure that our staff have the skills, training and technology to do their best work.How you'l fit in at EROAD is important as well. We really proud of our company culture and finding people with the right attitude is just as essential as a great CV.Working at EROAD means learning from people who are experts in their field. It also means working hard? we have to, to be able to grow as fast as we need to! But we encourage a healthy work/life balance and our low staff turnover tells us we probably got the balance right.We have staff from all over the globe? 25 countries at last count? and we may just have the highest rate of boat/surfboard/windsurfer ownership of any technology company, anywhere.");
+  $("#desc").val("EROAD is a fully integrated technology, road charging and services provider. We have developed the first commercial electronic weight-mile tax service to support transport carriers to better manage their Oregon regulatory obligations. The EROAD application generates weight-mile tax records, automatically records tax exempt mileage, supports configuration changes, calculates taxes owed, and provides a wide range of commercial and regulatory services including IFTA and IRP reporting. The company electronic weight-mileage tax solution has been approved by the State of Oregon and is supported by the Oregon Trucking Association.EROAD launched its innovative product in the Northwest region in early 2014, and strong customer uptake means that we are now looking to expand our team. We are looking for Customer Service Assistant to:Support the delivery of a professionally managed, efficient and effective customer service to our customers, sales executives and external dealers.Respond promptly to customer inquiries and provide a complete customer service experience and feedback loop for requests and improvement suggestions.Assist customers with training and any issues related to the EROAD solution, and diagnose and investigate incoming issues.Help create a culture within EROAD that is productive, open, honest, professional, innovative, friendly and sustainable.EROAD currently operates a help desk from 8:00 am until 5:00pm (Mon-Fri) with extended hours planned in the near future as our customer base in different regions expands. The Role reports to the Customer Service Manager and is based at the company office in Tualatin, Oregon.");
+  $("#benefits").val("Training:On-going training opportunities will be provided.Benefits:Market rate salary (plus company bonus scheme). Health benefits after qualifying period.");
+  $("#requirements").val("To excel in this role, candidates require:Friendly, helpful and open personality.Love of technology and always be striving to learn about product and service developments.Ability to pick up new skills and tasks quickly.Ideally one year or more in a help desk role in a fast moving technical environment.Excellent English written and verbal communication skills.Be an active listener who can show empathy and patience in a non-scripted environment.Strong organizational skills and attention to detail.Excellent time management skills.Highly adaptive and happy for role to change and develop.High professional and ethical standards.A good sense of humour and be prepared to go the extra mile as the company expands.");
+
+
+  $("#hasQuestion").find("input").removeAttr("checked");
+  $("#hasLogo").find("input").removeAttr("checked");
+  $("#telecommuting").find("input").removeAttr("checked");
+
+
+  $("#hasQuestion").find("input[value=0]").prop("checked", true);
+  $("#hasLogo").find("input[value=1]").prop("checked", true);
+  $("#telecommuting").find("input[value=0]").prop("checked", true);
+
+}
+
+function fillExample3(){
+  $("#title").val("Bilingual Retail Demonstrator");
+  $("#department").val("");
+  $("#employmentType").val("");
+  $("#requiredExp").val("");
+  $("#requiredEdu").val("");
+  $("#function").val("");
+  $("#industry").val("");
+  $("#companyProfile").val("");
+  $("#desc").val("dyson demonstrators work part time as members of the field sales team to engage consumers in an energetic confident manner provide them with knowledgeable information about dyson products in select retail locations i e costco bed bath andbeyond and best buy or events across the united states the most important part of a demonstrators job is to create a positive experience with the and the consumers assisting in the sale of dyson products to achieve company specified sales goals occur on saturdays sundays and some weekday holidays demonstrators must work a minimum of out of weekends per month within an assigned market we are in need of bilingual amp spanish demonstrators pay hr duties and responsibilities conduct planned demonstrations in assigned retail stores to achieve specified sales goals capture consumers attention to participate in high energy captivating in store demonstrations and model all aspects of dysons brand culture and passion for be a credible product and brand expert by educating consumers on the benefits and features of dyson products and actively listening to consumers to confidently recommend appropriate dyson products and comfortably over objections increase awareness and sales of dyson products through in store and event demonstrations actively dysons online reporting system to contribute quality and report attendance");
+  $("#benefits").val("opportunities for career growth employee salaries plus vesting in company k pension plan including a non elective contribution by dyson and a discretionary match on competitive contributions bonus schemes paid vacation including one additional day earned every year capped at weeks in total paid holidays immediate one floating holiday company paid life insurance health vision and dental insurance extended child care and parental leave of absence policies very relaxed dress code our paid short term and long term disability");
+  $("#requirements").val("qualifications needed hs diploma operating room or ged required experience in a retail sales environment required");
+
+  $("#hasQuestion").find("input").removeAttr("checked");
+  $("#hasLogo").find("input").removeAttr("checked");
+  $("#telecommuting").find("input").removeAttr("checked");
+
+  $("#hasQuestion").find("input[value=0]").prop("checked", true);
+  $("#hasLogo").find("input[value=0]").prop("checked", true);
+  $("#telecommuting").find("input[value=0]").prop("checked", true);
+
+}
