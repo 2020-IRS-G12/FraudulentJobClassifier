b555bd55f1a8a19583115f049994bae3c0ae434a
Your Name <you@example.com>
2020-10-23 10:15:50 +0800

    feat: add fraudulent job's report

--- Backend.py
+++ Backend.py
@@ -2,18 +2,34 @@ from flask import Flask
 from flask.globals import request
 from flask.helpers import url_for
 from flask.templating import render_template
-from werkzeug.utils import redirect
+from werkzeug.utils import html, redirect
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from job_projects import lr,be,ga_job,fliter
 import sys
 
+import HighlightSentence as hs
+
 import pandas as pd
 import numpy as np
+import nltk
+
+#nltk.download('punkt')
+#nltk.download('stopwords')
+
 
 app = Flask(__name__)
 
 data = pd.read_csv('data/data_lr.csv')
+
+class TextInfo:
+    word_list = []
+    company_profile = ''
+    requirements = ''
+    benefits = ''
+    description = ''
+
+text_info = TextInfo()
 
 @app.route('/')
 def start():
@@ -29,6 +45,12 @@ def showTheResult():
     bert_text = str(request.form['title'])+" "+str(request.form['department'])+" "+str(request.form['company_profile'])+" "+\
     str(request.form['description'])+" "+str(request.form['requirements'])+" "+str(request.form['benefits'])+" "+str(request.form['employment_type'])+" "+\
     str(request.form['required_experience'])+" "+str(request.form['required_education'])+" "+str(request.form['industry'])+" "+str(request.form['function'])+" "
+    
+    text_info.company_profile = str(request.form['company_profile'])
+    text_info.requirements = str(request.form['requirements'])
+    text_info.benefits = str(request.form['benefits'])
+    text_info.description = str(request.form['description'])
+    
     # if len(bert_text)<=20:
     #     bert_text = 'nan'
     bert_data =  pd.DataFrame([bert_text],columns=['text'])
@@ -48,8 +70,7 @@ def showTheResult():
     temp.append(request.form['industry'])
     temp.append(request.form['function'])
     lr_data = pd.DataFrame([temp],columns=data.columns.values)
-
-    word_list = []
+    
     if len(bert_text)==0:
         pjob = [0,1]
         job_last = 1
@@ -59,6 +80,7 @@ def showTheResult():
     elif fliter(bert_data):
         possi_lr = lr(lr_data)
         possi_b,word_list = be(bert_data)
+        text_info.word_list = word_list
         pjob,job_last = ga_job(possi_lr,possi_b)
     else:
         pjob = [0,1]
@@ -72,5 +94,14 @@ def showTheResult():
 
     return render_template('result.html',result = result_text, isFraudulent = job_last)
 
+@app.route('/report', methods=['POST','GET'])
+def showTheReport():
+
+    html_text = hs.highlightSentence('Company Profile',text_info.company_profile,text_info.word_list)
+    html_text = html_text+hs.highlightSentence('Requirements',text_info.requirements,text_info.word_list)
+    html_text = html_text+hs.highlightSentence('Benefits',text_info.benefits,text_info.word_list)
+    html_text = html_text+hs.highlightSentence('Description',text_info.description,text_info.word_list)
+    
+    return render_template('report.html',html_text=html_text)
 
 app.run(debug=False)
---
+++ HighlightSentence.py
@@ -0,0 +1,64 @@
+import nltk
+
+def highlightSentence(title,text,word_list):
+
+    if len(text)==0:
+        return ''
+
+    stopwords= ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",\
+            "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', \
+            'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their',\
+            'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', \
+            'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', \
+            'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', \
+            'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',\
+            'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',\
+            'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',\
+            'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', \
+            's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', \
+            've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',\
+            "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',\
+            "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", \
+            'won', "won't", 'wouldn', "wouldn't"]
+
+    new_list = []
+    for word in word_list:
+        if word.lower() not in stopwords:
+            new_list.append(word)
+    word_list = new_list
+    
+
+    sentence_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    sentences = sentence_tokenizer.tokenize(text)
+    
+    sentences_word_list = []
+    for sentence in sentences:
+        sentences_word_list.append(nltk.word_tokenize(sentence))
+
+    scores = []
+    for words in sentences_word_list:
+        count = 0
+        for word in words:
+
+            for keyword in word_list:
+                if word.lower() == keyword.lower():
+                    count = count+1
+            
+        scores.append(count)
+
+    html_text='<div> <div style=\'margin-left: 5px; margin-top: 10px;\'>'+title+':</div><div style=\'margin-left: 50px; margin-right: 10px;\'>'
+    for i in range(0,len(scores)):
+        class_txt = ''
+        if scores[i]<4:
+            class_txt = 'normal_alarm_text'
+        if scores[i]>=4 and scores[i]<8:
+            class_txt = 'middle_alarm_text'
+        if scores[i]>=8:
+            class_txt = 'high_alarm_text'
+        
+        html_text = html_text+'<span class=\''+class_txt+'\'>'+sentences[i]+'</span>'
+
+    html_text=html_text+'</div></div>'
+
+    return html_text
+
